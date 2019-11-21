88b6ec8058fecac4e6fb36e4b3fa3243a0ee28b4
Aylis <kenoptic@protonmail.com>
2019-11-20 18:45:28 -0600

    Map Update

--- public/js/map.js
+++ public/js/map.js
@@ -9,14 +9,23 @@
 // For now, just have a string:
 var testMap = [];
 var row0 = ['1', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '4'];
+
 var row1 = ['1', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '4'];
+
 var row2 = ['1', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ', '4'];
+
 var row3 = ['1', ' ', ' ', ' ', 'W', ' ', ' ', 'O', ' ', '4'];
+
 var row4 = ['1', ' ', ' ', ' ', 'W', ' ', ' ', ' ', ' ', '4'];
+
 var row5 = ['W', 'W', 'W', ' ', 'W', ' ', ' ', 'W', 'W', 'W'];
+
 var row6 = ['3', 'O', ' ', ' ', 'W', ' ', ' ', ' ', ' ', '2'];
+
 var row7 = ['3', ' ', ' ', ' ', 'W', ' ', 'O', ' ', ' ', '2'];
+
 var row8 = ['3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '2'];
+
 var row9 = ['3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '2'];
 
 testMap.push(row0);
@@ -34,21 +43,27 @@ testMap.push(row9);
 class Map {
    // Takes a text file and interprets the characters in it to create an array of the various spaces
    constructor(mapText) {
-      // Set up map dimensions
-      this.colCount = mapText.length;
-      this.rowCount = mapText[0].length;
-
       // Set up starting zone arrays
       this.sz_t1 = [];
       this.sz_t2 = [];
       this.sz_t3 = [];
       this.sz_t4 = [];
+      this.sz_t5 = [];
+      this.sz_t6 = [];
+      this.sz_t7 = [];
+      this.sz_t8 = [];
+      this.sz_t9 = [];
 
       // Set up wall/obstacle array
       this.features = [];
+      this.dropZone = [];
 
       // Load the map
-      // TODO: LOAD TXT FILE
+      // TODO: LOAD TXT FILE INTO ARRAY OF ROW ARRAYS HOLDING COLUMNS
+
+      // Set up map dimensions
+      this.colCount = mapText.length;
+      this.rowCount = mapText[0].length;
 
       // Create the objects
       mapText.forEach(row, idx => {
@@ -69,10 +84,22 @@ class Map {
                this.sz_t3.push([x, y]);
             else if (cell == 4)
                this.sz_t4.push([x, y]);
+            else if (cell == 5)
+               this.sz_t5.push([x, y]);
+            else if (cell == 6)
+               this.sz_t6.push([x, y]);
+            else if (cell == 7)
+               this.sz_t7.push([x, y]);
+            else if (cell == 8)
+               this.sz_t8.push([x, y]);
+            else if (cell == 9)
+               this.sz_t9.push([x, y]);
             else if (cell == 'W')
                this.features.push(new Wall(x, y));
             else if (cell == 'O')
                this.features.push(new Obstacle(x, y));
+            else if (cell == 'D')
+               this.dropZone.push(new Drop(x, y));
          });
       });
    }
